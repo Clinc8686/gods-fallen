e0ec75202704de96e0fa6d75580540d6040fe555
Mario <mario1.lanpert@gmail.com>
2022-09-13 13:07:29 +0200

    fixed Enemy_4 collision Bug

--- Assets/Prefabs/Enemy/Enemy_Collision.cs
+++ Assets/Prefabs/Enemy/Enemy_Collision.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy_Collision : MonoBehaviour
 {
     public float life = 2;
     private void OnCollisionEnter2D(Collision2D col)
