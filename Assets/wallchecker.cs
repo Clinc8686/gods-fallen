5aae100b67dff36effe8acb10d407cd9950488f4
fabtomsch <schollfabiantom@gmail.com>
2022-09-15 16:58:43 +0200

    Enemy2

--- Assets/wallchecker.cs
+++ Assets/wallchecker.cs
@@ -3,34 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-
-public class Enemy_Movement : MonoBehaviour
+public class wallchecker : MonoBehaviour
 {
- [SerializeField] float movementSpeed = 1f;
+    public Collider2D body;
+    [SerializeField] float movementSpeed = 1f;
     Rigidbody2D rb;
- 
-
-
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-       
+
 
 
     }
-
-    // Update is called once per frame
     void Update()
     {
-        if(IsfacingRight())
+        if (IsfacingRight())
         {
-          
-            rb.velocity = new Vector2(movementSpeed,0f);
+
+            rb.velocity = new Vector2(movementSpeed, 0f);
         }
         else
         {
-           
+
             rb.velocity = new Vector2(-movementSpeed, 0f);
         }
     }
@@ -39,21 +33,12 @@ public class Enemy_Movement : MonoBehaviour
         return transform.localScale.x > Mathf.Epsilon;
     }
 
-    private void OnTriggerExit2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-        if (collision.gameObject.name != "Player" )
+        if (collision.gameObject.name != "Player")
         {
             transform.localScale = new Vector2(-(Mathf.Sign(rb.velocity.x)), transform.localScale.y);
         }
-       
+
     }
-
- 
-
-
-
-
-
-
 }
