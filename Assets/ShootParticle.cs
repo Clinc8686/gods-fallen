7272e2d2f4548b04dfc01345f366333b24c79793
Alizzie <79795653+Alizzie@users.noreply.github.com>
2022-09-15 14:01:31 +0200

    Enemy Life Health and Dmg

--- Assets/ShootParticle.cs
+++ Assets/ShootParticle.cs
@@ -1,15 +1,19 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ShootParticle : MonoBehaviour
 {
     [SerializeField] private ParticleSystem pS;
-    private List<ParticleCollisionEvent> collisionEvents = new List<ParticleCollisionEvent>();
+    [SerializeField] private float damage = 1;
 
     private void OnParticleCollision(GameObject other)
     {
-        
+        if (other.tag == "Enemy")
+        {
+            other.GetComponent<Enemy_Life>().takeLife(damage);
+        }
     }
 }
