365475f794557f306d6befcc2b9c41ab4b78dada
Mario <mario1.lanpert@gmail.com>
2022-09-16 19:52:50 +0200

    Changed Scenes and comment out enemy sounds

--- Assets/EnemyGFX.cs
+++ Assets/EnemyGFX.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Pathfinding;
+using UnityEngine.LowLevel;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
 
 public class EnemyGFX : MonoBehaviour
 {
     public AIPath aiPath;
+    [SerializeField] private AudioSource enemySource;
 
+    private void Start()
+    { 
+        PlayLoop();
+    }
 
-    // Update is called once per frame
     void Update()
     {
         if (aiPath.desiredVelocity.x >= 0.01f)
@@ -17,8 +27,22 @@ public class EnemyGFX : MonoBehaviour
         }
         else if (aiPath.desiredVelocity.x <= -0.01f) {
             transform.localScale = new Vector3(1f,1f,1f);
-        
+        }
     }
-}
+
+    void PlayLoop()
+    {
+        int r = Random.Range(15, 100);
+        //StartCoroutine(WaitSeconds(r));
+    }
+
+    /*IEnumerator WaitSeconds(int r)
+    {
+        while (true)
+        {
+            enemySource.Play();
+            yield return new WaitForSeconds(r);
+        }
+    }*/
 }
 
