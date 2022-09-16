2e2f529bb10fc2b18995dac51fcbe5ad7ad5c28b
Alizzie <79795653+Alizzie@users.noreply.github.com>
2022-09-16 15:49:20 +0200

    Changing Players Face while moving

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,13 +8,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5;
+    [SerializeField] private Sprite sideFace;
+    [SerializeField] private Sprite frontFace;
+    [SerializeField] private float changeTime = 0.5f;
+    private float changeFace;
     private Vector2 moveDirection;
 
     private void OnMove(InputValue value)
     {
+        gameObject.GetComponent<SpriteRenderer>().sprite = sideFace;
         moveDirection = value.Get<Vector2>();
-        //Debug.Log(moveDirection.x);
-
+        Debug.Log(moveDirection.x + " " + transform.localEulerAngles + " " + gameObject.GetComponent<SpriteRenderer>().sprite.name);
+        
         if (moveDirection.x < 0)
         {
             transform.rotation = Quaternion.Euler(0,-180,0);
@@ -23,11 +28,22 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.rotation = Quaternion.Euler(0,0,0);
         }
+        
+        if(moveDirection.x != 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = sideFace;
+            changeFace = changeTime;
+        }
     }
 
     private void Update()
     {
         Vector3 dirVec = new Vector3(moveDirection.x, 0, 0);
         transform.position += dirVec * (moveSpeed * Time.deltaTime);
+        if (moveDirection.x == 0 && (changeFace <= 0)) 
+        {
+            gameObject.GetComponent<SpriteRenderer>().sprite = frontFace;
+        }
+        changeFace -= Time.deltaTime;
     }
 }
