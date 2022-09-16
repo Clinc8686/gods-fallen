aa220ad197356363a9a717fbb57b2e44e793a461
fabtomsch <schollfabiantom@gmail.com>
2022-09-16 19:57:26 +0200

    Light versucht

--- Assets/Prefabs/BulletColor.cs
+++ Assets/Prefabs/BulletColor.cs
@@ -9,21 +9,47 @@ public class BulletColor : MonoBehaviour
     [SerializeField] private Sprite sky;
     [SerializeField] private Sprite hell;
 
+
+
+    public Transform Light;
+
+
+    private void Start()
+    {
+        
+        
+    }
     void Update()
     {
+        
         float playerHeight = transform.position.y;
         if (playerHeight > PlayerAnimations.skyDepth && playerHeight < PlayerAnimations.skyHeigth)
         {
             GetComponent<SpriteRenderer>().sprite = sky;
+            Light = transform.FindChild("LightLeere");
+            Light.gameObject.SetActive(true);
         } else if (playerHeight > PlayerAnimations.earthDepth && playerHeight < PlayerAnimations.earthHeigth)
         {
             GetComponent<SpriteRenderer>().sprite = earth;
+            
+            Light.gameObject.SetActive(false);
+            Light = transform.FindChild("LightHell");
+            Light.gameObject.SetActive(true);
+
         } else if (playerHeight > PlayerAnimations.hellDepth && playerHeight < PlayerAnimations.hellHeight)
         {
             GetComponent<SpriteRenderer>().sprite = hell;
+            Light.gameObject.SetActive(false);
+            Light = transform.FindChild("LightEarth");
+            Light.gameObject.SetActive(true);
+
         } else if (playerHeight > PlayerAnimations.emptinessDepth && playerHeight < PlayerAnimations.emptinessHeight)
         {
             GetComponent<SpriteRenderer>().sprite = emptiness;
+            Light.gameObject.SetActive(false);
+            Light = transform.FindChild("LightHeaven");
+            Light.gameObject.SetActive(true);
+
         }
     }
 }
