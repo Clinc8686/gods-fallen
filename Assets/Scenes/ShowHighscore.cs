68a992ad03e87ad905ad13da339a805be51ace6a
Alizzie <79795653+Alizzie@users.noreply.github.com>
2022-09-18 09:51:28 +0200

    Player side face move

--- Assets/Scenes/ShowHighscore.cs
+++ Assets/Scenes/ShowHighscore.cs
@@ -8,7 +8,7 @@ public class ShowHighscore : MonoBehaviour
     [SerializeField] private TextMeshProUGUI score;
     void Start()
     {
-        if (PlayerPrefs.HasKey("Highscore"))
+        if (PlayerPrefs.HasKey("Highscore") && score != null)
         {
             int time = (int) PlayerPrefs.GetFloat("Highscore");
             score.SetText(time + " seconds");
